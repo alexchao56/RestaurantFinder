4e611e86acda26a572acdeb701786221a05b0788
Alex Chao <alexchao56@gmail.com>
2014-10-12 16:25:38 -0700

    Updated data processing script

--- util.R
+++ util.R
@@ -39,3 +39,39 @@ unique(business_subset$city[business_subset$state == "NV"])
 length(business_subset$city[business_subset$city == "Anthem"])
 
 length(grep(pattern = "Las Vegas", x = business_subset$city))
+
+
+
+## Getting Waterloo
+waterloo = business_subset[business_subset$city == "Waterloo",]
+waterloo_attributes = waterloo$attributes
+waterloo_hours = waterloo$hours
+#Removing hours
+waterloo = waterloo[,-3]
+#Removing attributes
+waterloo = waterloo[,-13]
+#Removing categories
+waterloo = waterloo[,-4]
+#Removing neighborhoods
+waterloo = waterloo[,-7]
+waterloo$reservations = waterloo_attributes$"Takes Reservations"
+waterloo$wifi = waterloo_attributes$"Wi-Fi"
+waterloo$takeout = waterloo_attributes$"Take-out"
+waterloo$creditCards = waterloo_attributes$"Accepts Credit Cards"
+
+# Fixing NA values for logical vectors
+for (i in 1:ncol(waterloo)) {
+    if ((class(waterloo[,i]) == "logical")) {
+        waterloo[,i][is.na(waterloo[,i])] = FALSE
+    }
+}
+
+#Fixing NA values for wifi
+waterloo$wifi[is.na(waterloo$wifi)] = "no"
+
+#Unlisting the Credit Cards
+waterloo$creditCards[sapply(waterloo$creditCards, is.null)] = FALSE
+waterloo$creditCards = unlist(waterloo$creditCards)
+    
+write.csv(waterloo, "waterlooAttributes.csv")
+write.csv(waterloo_hours, "waterlooHours.csv")
